5c185bed009a101eef691ba542fed89d4426ee0f
gk022135 <gk022135@gmail.com>
2025-02-04 22:58:16 +0530

    hi

--- index.js
+++ index.js
@@ -13,17 +13,11 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
-
-app.get('/check', (req, res) => {
-    res.send("hi it working index.js");
-    console.log("Check route hit");
-});
-
 app.use(bodyParser.json());
 app.use(cors());
 
 
-app.use('/auth', SinLog);
+app.use('/qr-code-system', SinLog);
 
 
 app.listen(PORT, () => {
